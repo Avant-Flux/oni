b01f7f2eaf333ce83f745614ff98b10976873478
Raven <RavensKrag@gmail.com>
2012-11-17 11:43:14 -0500

    Define functions in for window with proper arity.  Also, defer add_time() to ruby level.

--- ext/OgreRuby/window.c
+++ ext/OgreRuby/window.c
@@ -4,7 +4,8 @@ VALUE Init_OgreWindow(VALUE outer){
 	VALUE klass = rb_define_class_under(outer, "Window", rb_cObject);
 	
 	rb_define_method(klass, "initialize", initialize, -1);
-	rb_define_method(klass, "show", show, -1);
+	rb_define_method(klass, "show", show, 0);
+	rb_define_method(klass, "add_time", add_time, 1);
 }
 
 static VALUE initialize(int argc, VALUE *argv, VALUE klass){
@@ -15,6 +16,10 @@ static VALUE finalize(int argc, VALUE *argv, VALUE klass){
 	
 }
 
-static VALUE show(int argc, VALUE *argv, VALUE klass){
+static VALUE show(VALUE klass){
 	
+}
+
+static VALUE add_time(VALUE klass, VALUE dt){
+	rb_funcall(klass, rb_intern("add_time"), 1, dt);
 }
--- ext/OgreRuby/window.h
+++ ext/OgreRuby/window.h
@@ -3,4 +3,5 @@ VALUE Init_OgreWindow(VALUE outer);
 static VALUE initialize(int argc, VALUE *argv, VALUE klass);
 static VALUE finalize(int argc, VALUE *argv, VALUE klass);
 
-static VALUE show(int argc, VALUE *argv, VALUE klass);
+static VALUE show(VALUE klass);
+static VALUE add_time(VALUE klass, VALUE dt);
