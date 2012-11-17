75ac86de0a66606f75a6f1438b92a6352fffca45
Raven <RavensKrag@gmail.com>
2012-11-17 13:13:20 -0500

    Rename "klass" to "self" where appropriate for Window.

--- ext/OgreRuby/window.c
+++ ext/OgreRuby/window.c
@@ -8,21 +8,21 @@ VALUE Init_OgreWindow(VALUE outer){
 	rb_define_method(klass, "add_time", add_time, 1);
 }
 
-static VALUE initialize(int argc, VALUE *argv, VALUE klass){
-	OGRE_TYPE Agent = Ogre_Agent(); // MUST free memory later
+static VALUE initialize(int argc, VALUE *argv, VALUE self){
+	// OGRE_TYPE Agent = Ogre_Agent(); // MUST free memory later
 	
 	// Allocate space for a OGRE_WINDOW (pointer)
 	// Initialize the window
 }
 
-static VALUE finalize(int argc, VALUE *argv, VALUE klass){
+static VALUE finalize(int argc, VALUE *argv, VALUE self){
 	// Free space used by the window
 }
 
-static VALUE show(VALUE klass){
+static VALUE show(VALUE self){
 	// Run the window
 }
 
-static VALUE add_time(VALUE klass, VALUE dt){
-	rb_funcall(klass, rb_intern("add_time"), 1, dt);
+static VALUE add_time(VALUE self, VALUE dt){
+	rb_funcall(self, rb_intern("add_time"), 1, dt);
 }
