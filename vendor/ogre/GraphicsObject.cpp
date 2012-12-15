5ccc6d993b88afda8ae1c6474358b6d09303a35f
Raven <RavensKrag@gmail.com>
2012-12-15 15:13:45 -0500

    Create GraphicsObject class which will be the base of all interactions with Ogre.

---
+++ vendor/ogre/GraphicsObject.cpp
@@ -0,0 +1,88 @@
+#include "GraphicsObject.h"
+
+GraphicsObject::GraphicsObject(std::string name, std::string filename)
+{
+	using namespace Ogre;
+	if (sceneMgr == NULL)
+		return;
+	
+	mNode = sceneMgr->getRootSceneNode()->createChildSceneNode();
+	mEntity = sceneMgr->createEntity(name, filename);
+	mNode->attachObject(mEntity);
+	
+	mNode->setPosition(0,0,0); // make the Ogre stand on the plane
+}
+
+GraphicsObject::~GraphicsObject()
+{
+	
+}
+
+void
+GraphicsObject::update(Ogre::Real deltaTime)
+{
+	
+}
+
+// Position
+void
+GraphicsObject::setPosition(const Ogre::Vector3 &pos)
+{
+	mNode->setPosition(pos);
+}
+
+void
+GraphicsObject::setPosition(Ogre::Real x, Ogre::Real y, Ogre::Real z)
+{
+	mNode->setPosition(x,y,z);
+}
+
+
+void
+GraphicsObject::translate(const Ogre::Vector3 &d, Ogre::Node::TransformSpace relativeTo)
+{
+	// Moves the node along the Cartesian axes. 
+	mNode->translate(d, relativeTo);
+}
+
+void
+GraphicsObject::translate(Ogre::Real x, Ogre::Real y, Ogre::Real z, Ogre::Node::TransformSpace relativeTo)
+{
+	// Moves the node along the Cartesian axes. 
+	mNode->translate(x,y,z, relativeTo);
+}
+
+void
+GraphicsObject::translate(const Ogre::Matrix3 &axes, const Ogre::Vector3 &move, Ogre::Node::TransformSpace relativeTo)
+{
+	// Moves the node along arbitrary axes. 
+	mNode->translate(axes, move, relativeTo);
+}
+
+void
+GraphicsObject::translate(const Ogre::Matrix3 &axes, Ogre::Real x, Ogre::Real y, Ogre::Real z, Ogre::Node::TransformSpace relativeTo)
+{
+	// Moves the node along arbitrary axes. 
+	mNode->translate(axes, x,y,z, relativeTo);
+}
+	
+
+
+// Rotation
+void
+GraphicsObject::pitch(const Ogre::Radian &angle, Ogre::Node::TransformSpace relativeTo)
+{
+	
+}
+
+void
+GraphicsObject::yaw(const Ogre::Radian &angle, Ogre::Node::TransformSpace relativeTo)
+{
+	
+}
+
+void
+GraphicsObject::roll(const Ogre::Radian &angle, Ogre::Node::TransformSpace relativeTo)
+{
+	
+}
---
+++ vendor/ogre/GraphicsObject.h
@@ -0,0 +1,36 @@
+#ifndef GRAPHICS_OBJECT_H
+#define GRAPHICS_OBJECT_H
+
+#include "BaseApplication.h"
+#include <deque>
+
+extern Ogre::SceneManager* sceneMgr;	// Defined in main.cpp
+
+class GraphicsObject
+{
+public:
+	GraphicsObject(std::string name, std::string filename);
+	virtual ~GraphicsObject();
+	
+	virtual void update(Ogre::Real deltaTime);		// update the game object
+	
+	// Position
+	void setPosition(const Ogre::Vector3 &pos);
+	void setPosition(Ogre::Real x, Ogre::Real y, Ogre::Real z);
+	
+	void translate(const Ogre::Vector3 &d, Ogre::Node::TransformSpace relativeTo=Ogre::Node::TS_PARENT);
+	void translate(Ogre::Real x, Ogre::Real y, Ogre::Real z, Ogre::Node::TransformSpace relativeTo=Ogre::Node::TS_PARENT);
+	void translate(const Ogre::Matrix3 &axes, const Ogre::Vector3 &move, Ogre::Node::TransformSpace relativeTo=Ogre::Node::TS_PARENT);
+	void translate(const Ogre::Matrix3 &axes, Ogre::Real x, Ogre::Real y, Ogre::Real z, Ogre::Node::TransformSpace relativeTo=Ogre::Node::TS_PARENT);
+	
+	// Rotation
+	void pitch(const Ogre::Radian &angle, Ogre::Node::TransformSpace relativeTo=Ogre::Node::TS_LOCAL);
+	void yaw(const Ogre::Radian &angle, Ogre::Node::TransformSpace relativeTo=Ogre::Node::TS_LOCAL);
+	void roll(const Ogre::Radian &angle, Ogre::Node::TransformSpace relativeTo=Ogre::Node::TS_LOCAL);
+
+protected:
+	Ogre::SceneNode* mNode;
+	Ogre::Entity* mEntity;
+};
+
+#endif /* GRAPHICS_OBJECT_H */
