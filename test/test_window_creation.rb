0cdda5c97847fb39f2f4e6b81ff73b15629187fa
Raven <RavensKrag@gmail.com>
2012-12-21 20:21:23 -0500

    Make window creation test evaluate only the most basic window setup.

--- test/test_window_creation.rb
+++ test/test_window_creation.rb
@@ -5,22 +5,4 @@ require 'glu'
 
 require 'OgreRuby'
 
-# class Window < OgreRuby::Window
-# 	def initialize
-		
-# 	end
-	
-# 	def add_time(dt)
-		
-# 	end
-	
-# 	# Ideally, would be able to draw in a 2D context
-# 	# on top of Ogre3D rendering in this method.
-# 	def draw
-		
-# 	end
-# end
-
-# Window.new.show
-# Window.new
 OgreRuby::Window.new.show
