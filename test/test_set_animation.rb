652208ff9ceef1042cc2ecf9f9635ac74c56ecc4
Raven <RavensKrag@gmail.com>
2012-12-29 14:00:15 -0500

    Change animation setting test to use human character model.

--- test/test_set_animation.rb
+++ test/test_set_animation.rb
@@ -5,11 +5,12 @@ require 'glu'
 
 class Window < OgreRuby::Window
 	def setup
-		@a = OgreRuby::Agent.new(self, "Golem", "Golem.mesh")
+		@a = OgreRuby::Agent.new(self, "MainCharacter", "MainCharacter.mesh")
 		@a.translate(0, 0, 0)
 		
-		@a.top_animation = "my_animation"
-		# @a.top_animation = ""
+		# @a.top_animation = "my_animation"
+		@a.base_animation = "walkywalky"
+		@a.top_animation = "flippyfloppy"
 	end
 	
 	def update(dt)
