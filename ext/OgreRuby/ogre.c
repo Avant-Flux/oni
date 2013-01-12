77e3b34570cbd579cd0a2e8942d8bca8d4eb77e0
Raven <RavensKrag@gmail.com>
2013-01-12 16:42:29 -0500

    Use EXTERN instead of including headers to remove symbol collison when headers with STATIC funcitons are loaded.

--- ext/OgreRuby/ogre.c
+++ ext/OgreRuby/ogre.c
@@ -4,9 +4,10 @@
 
 #include <OgreInterface.h>
 
-#include "ogre.h"
-#include "window.h"
-#include "agent.h"
+// Use extern instead of including headers, as the headers only
+// have one function each which is not declared "extern"
+extern Init_OgreWindow(VALUE module);	// Defined in window.h
+extern Init_OgreAgent(VALUE module);	// Defined in agent.h
 
 void Init_OgreRuby(){
 	VALUE module = rb_define_module("OgreRuby");
