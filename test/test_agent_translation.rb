120068b34e6824f9777ac6bb90c971de0a74d92a
Raven <RavensKrag@gmail.com>
2013-01-16 22:49:07 -0500

    Update translation test so movement buttons can be held down.

--- test/test_agent_translation.rb
+++ test/test_agent_translation.rb
@@ -7,16 +7,52 @@ require 'OgreRuby'
 
 class Window < OgreRuby::Window
 	def setup
-		@a = OgreRuby::Agent.new(self, "MainCharacter", "MainCharacter.mesh")
+		# Implies that this is where the camera is made
+		# This only creates a handle for the camera
+		@camera = OgreRuby::Camera.new(self, "main_camera", 0) # TODO: Make z_order=0 by default
+		
+		@camera.fov = 110
+		
+		scale = 1.5
+		
+		r = 3*scale
+		angle = 22.degrees
+		
+		x = r*Math.sin(angle)
+		z = r*Math.cos(angle)
+		@camera.position = [x,4*scale,z]
+
+		@camera.look_at [0,0,0]
+		@camera.near_clip_distance = 1
+		
+		
+		@a = OgreRuby::Agent.new(self, "Human_Male", "Human_Male.mesh")
 		@a.translate(0, 0, 0)
 		
-		# @a.top_animation = "my_animation"
-		@a.base_animation = "walkywalky"
+		@a.base_animation = "my_animation"
+		# @a.base_animation = "walkywalky"
 		# @a.top_animation = "flippyfloppy"
+		
+		@moving = false
+		@move_constant = 0.1
+		@move_direction = nil
 	end
 	
 	def update(dt)
 		@a.update dt
+		
+		if @moving
+			case @move_direction
+				when :right
+					@a.translate(@move_constant,0,0)
+				when :left
+					@a.translate(-@move_constant,0,0)
+				when :up
+					@a.translate(0,0,-@move_constant)	# into scene
+				when :down
+					@a.translate(0,0,@move_constant)	# out of scene
+			end
+		end
 	end
 	
 	def draw
@@ -38,21 +74,33 @@ class Window < OgreRuby::Window
 				@a.position = [0,0,0]
 		end
 		
-		case id
-			when 205 # right
-				@a.translate(1,0,0)
-			when 203 # left
-				@a.translate(-1,0,0)
-			when 200 #up
-				@a.translate(0,0,-1)	# into scene
-			when 208 #down
-				@a.translate(0,0,1)		# out of scene
+		
+		if id == 205 # right
+			@move_direction = :right
+			@moving = true
+		end
+		
+		if id == 203 # left
+			@move_direction = :left
+			@moving = true
+		end
+		
+		if id == 200 #up
+			@move_direction = :up
+			@moving = true
+		end
+		
+		if id == 208 #down
+			@move_direction = :down
+			@moving = true
 		end
 	end
 	
 	def button_up(id)
 		# puts "up: #{button_id_to_sym(id)}"
-		
+		if id == 205 || id == 203 || id == 200 || id == 208
+			@moving = false
+		end
 	end
 end
 
